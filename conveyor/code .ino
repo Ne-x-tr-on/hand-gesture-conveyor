a032dc4997984f1cfd4e6c7493dbd86034f8efcd
Ne-x-tr-on <26nextron.dev@gmail.com>
2025-12-18 00:07:16 +0300

    _

--- conveyor/code .ino
+++ conveyor/code .ino
@@ -1,24 +1,24 @@
-const int ledPin = 13; // Change if using other output
-const int testPin = LED_BUILTIN;
-String command;
+// const int ledPin = 13; // Change if using other output
+// const int testPin = LED_BUILTIN;
+// String command;
 
-void setup() {
-  pinMode(ledPin, OUTPUT);
-  pinMode(testPin, OUTPUT);
-  Serial.begin(9600);
-}
+// void setup() {
+//   pinMode(ledPin, OUTPUT);
+//   pinMode(testPin, OUTPUT);
+//   Serial.begin(9600);
+// }
 
-void loop() {
-  if (Serial.available() > 0) {
-    command = Serial.readStringUntil('\n');
-    command.trim(); // Remove any whitespace or newline
+// void loop() {
+//   if (Serial.available() > 0) {
+//     command = Serial.readStringUntil('\n');
+//     command.trim(); // Remove any whitespace or newline
 
-    if (command == "ON") {
-      digitalWrite(ledPin, HIGH);
-      digitalWrite(testPin, HIGH);
-    } else if (command == "OFF") {
-      digitalWrite(ledPin, LOW);
-      digitalWrite(testPin, LOW);
-    }
-  }
-}
+//     if (command == "ON") {
+//       digitalWrite(ledPin, HIGH);
+//       digitalWrite(testPin, HIGH);
+//     } else if (command == "OFF") {
+//       digitalWrite(ledPin, LOW);
+//       digitalWrite(testPin, LOW);
+//     }
+//   }
+// }
