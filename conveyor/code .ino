581c1427ea8059c598bb5e4ed5cb358d96c2f33f
Ne-x-tr-on <26nextron.dev@gmail.com>
2025-12-17 20:53:50 +0300

    Update Pin

--- conveyor/code .ino
+++ conveyor/code .ino
@@ -1,8 +1,10 @@
 const int ledPin = 13; // Change if using other output
+const int testPin = LED_BUILTIN;
 String command;
 
 void setup() {
   pinMode(ledPin, OUTPUT);
+  pinMode(testPin, OUTPUT);
   Serial.begin(9600);
 }
 
@@ -13,8 +15,10 @@ void loop() {
 
     if (command == "ON") {
       digitalWrite(ledPin, HIGH);
+      digitalWrite(testPin, HIGH);
     } else if (command == "OFF") {
       digitalWrite(ledPin, LOW);
+      digitalWrite(testPin, LOW);
     }
   }
 }
